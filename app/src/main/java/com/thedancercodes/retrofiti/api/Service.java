16dffe33fe1996831421de0a4e72eb0198e2a2fa
TheDancerCodes <rojtaracha@gmail.com>
2018-02-14 16:41:03 +0300

    [CHORE] Import MoviesResponse to Service Interface.

--- app/src/main/java/com/thedancercodes/retrofiti/api/Service.java
+++ app/src/main/java/com/thedancercodes/retrofiti/api/Service.java
@@ -1,5 +1,7 @@
 package com.thedancercodes.retrofiti.api;
 
+import com.thedancercodes.retrofiti.model.MoviesResponse;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
