d33a0307a52d944d38a5e32407d8c160404aa196
TheDancerCodes <rojtaracha@gmail.com>
2018-02-15 12:18:33 +0300

    [FEATURE] Main Activity SetUp
    [+] Created the getActivity(), initViews() and loadJSON() methods.

--- app/src/main/java/com/thedancercodes/retrofiti/MainActivity.java
+++ app/src/main/java/com/thedancercodes/retrofiti/MainActivity.java
@@ -1,13 +1,93 @@
 package com.thedancercodes.retrofiti;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.res.Configuration;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.thedancercodes.retrofiti.adapter.MoviesAdapter;
+import com.thedancercodes.retrofiti.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Create the field variables
+    private RecyclerView mRecyclerView;
+    private MoviesAdapter adapter;
+    private List<Movie> mMovieList;
+    ProgressDialog pd;
+    private SwipeRefreshLayout swipeContainer;
+    public static final String LOG_TAG = MoviesAdapter.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.main_content);
+        swipeContainer.setColorSchemeResources(android.R.color.holo_orange_dark);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                initViews();
+                Toast.makeText(MainActivity.this, "Movies Refreshed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    public Activity getActivity() {
+        Context context = this;
+        while (context instanceof ContextWrapper) {
+            if (context instanceof Activity) {
+                return (Activity) context;
+            }
+            context = ((ContextWrapper) context).getBaseContext();
+        }
+        return null;
+    }
+
+    // This method is responsible for initializing the views.
+    private void initViews() {
+        pd = new ProgressDialog(this);
+        pd.setMessage("Fetching movies...");
+        pd.setCancelable(false);
+        pd.show();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mMovieList = new ArrayList<>();
+        adapter = new MoviesAdapter(this, mMovieList);
+
+        // Set the Orientation
+        if (getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        } else {
+            mRecyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+        }
+
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        // Loading JSON
+        loadJSON();
+    }
+
+    // This method is responsible for loading JSON data.
+    private void loadJSON() {
+
+    }
+
 }
